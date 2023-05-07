620e5ce9e6fd3659010ff9f1242bdb7040417470
Petrixx <75099491+Petrix07@users.noreply.github.com>
2023-05-07 20:04:57 -0300

    Criada as rotas de pessoas e contatos

--- routes/pages.php
+++ routes/pages.php
@@ -4,6 +4,7 @@ use \App\Http\Response,
     \App\Controller\Pages;
 
 /* Rota da página Home */
+
 $obRouter->get('/', [
     function () {
         return new Response(200, Pages\Home::getHome());
@@ -13,13 +14,27 @@ $obRouter->get('/', [
 /* Rota da página Sobre */
 $obRouter->get('/sobre', [
     function () {
-        return new Response(200, Pages\Sobre::getHome());
+        return new Response(200, Pages\Sobre::getAbout  ());
     }
 ]);
 
 /* Rota dinâmica */
-$obRouter->get('/pagina/{idPagina}', [
-    function ($idPagina) {
-        return new Response(200, "Página $idPagina");
+$obRouter->get('/pessoas', [
+    function () {
+        return new Response(200, 'Página Pessoas');
+    }
+]);
+
+/* Rota dinâmica */
+$obRouter->get('/contatoss', [
+    function () {
+        return new Response(200, 'Página Contatos');
+    }
+]);
+
+/* Rota dinâmica */
+$obRouter->get('/pagina/{idPagina}/{acao}', [
+    function ($idPagina, $acao) {
+        return new Response(200, 'Página' . $idPagina . $acao);
     }
 ]);
